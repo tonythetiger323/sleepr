dd67bc7de87de6451b8c574fdb784ea590457b1c
Tony Greeley <tony.greeley@me.com>
2023-07-21 20:07:57 -0500

    Updated setup for pushing to GCP

--- libs/common/src/decorators/current-user.decorator.ts
+++ libs/common/src/decorators/current-user.decorator.ts
@@ -1,7 +1,7 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
-import { UserDocument } from '../../../../apps/auth/src/users/models/user.schema';
+import { UserDocument } from '../models/user.schema';
 
-const getCurrentUserByContext = (context: ExecutionContext) => {
+const getCurrentUserByContext = (context: ExecutionContext): UserDocument => {
   return context.switchToHttp().getRequest().user;
 };
 export const CurrentUser = createParamDecorator(
