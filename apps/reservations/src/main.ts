71b27853ab541a52e9d4ac26535ffd5889666de6
Tony Greeley <tony.greeley@me.com>
2023-07-24 19:25:46 -0500

    Added global prefix of /reservations to auth and reservations main.ts files.

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
+  app.setGlobalPrefix('/reservations');
   app.use(cookieParser());
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
